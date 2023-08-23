0576756bc5a84d280c90afd4b6c6ecbd56745876
Faye Grace <ren>
2023-08-23 18:37:48 +0300

    empty input and parsing it

--- main.c
+++ main.c
@@ -1,32 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define WIDTH 80
-#define HEIGHT 30
+#define WIDTH 40
+#define HEIGHT 20
 
-int ** memall() {
-    int ** input = malloc(sizeof(char *) * HEIGHT);
-    for(int i = 0; i < WIDTH; i++) input[i] = malloc(sizeof(char));
-    for(int i = 0; i < HEIGHT; i++) {
-        for(int j = 0; j < WIDTH; j++) {
-            input[i][j] = 1;
-        }
-    }
+char ** memall() {
+    char ** input = malloc(sizeof(char *) * HEIGHT);
+    for(int i = 0; i < HEIGHT; i++) input[i] = malloc(sizeof(char) * (WIDTH + 1));
     return input;
 }
 
-void output(int ** input) {
+void init(char ** input) {
+    for(int i = 0; i < HEIGHT; i++) {
+        for(int j = 0; j < WIDTH + 1; j++) {
+            scanf("%c", &(input)[i][j]);
+        }
+    }
+}
+
+void output(char ** input) {
     for(int i = 0; i < HEIGHT; i++) {
         for(int j = 0; j < WIDTH; j++) {
-            printf("%d", input[i][j]);
+            printf("%c", input[i][j]);
         }
         printf("\n");
     }
 }
 
 int main() {
-    int ** input;
+    char ** input;
+    /* for(int i = 0; i < HEIGHT; i++) { */
+    /*     for(int j = 0; j < WIDTH; j++) { */
+    /*         printf("*"); */
+    /*     } */
+    /*     printf("\n"); */
+    /* } */
     input = memall();
+    init(input);
     output(input);
+    for(int i = 0; i < HEIGHT; i++) free(input[i]);
     free(input);
     return 0;
 }
