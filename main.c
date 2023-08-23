022a6310861adef93672f2d8e5cc6f5adb336e16
Faye Grace <ren>
2023-08-23 18:56:28 +0300

    need to work on edge cases and do the game logic

--- main.c
+++ main.c
@@ -11,10 +11,61 @@ char ** memall() {
 
 void init(char ** input) {
     for(int i = 0; i < HEIGHT; i++) {
-        for(int j = 0; j < WIDTH + 1; j++) {
+        for(int j = 0; j < WIDTH + 1; j++) { // + 1 so it scans newlines;;
             scanf("%c", &(input)[i][j]);
         }
     }
+}
+
+int horizontal_flag(int j) {
+    if(j == 0) return -1;
+    if(j == WIDTH - 1) return 1;
+    return 0;
+}
+
+int vertical_flag(int i) {
+    if(i == 0) return -1;
+    if(i == HEIGHT - 1) return 1;
+    return 0;
+}
+
+int check_right(char ** input, int i, int j) {
+    int res = 0;
+    for(int s = 0; s < 3; s++) {
+        if(input[i - 1 + s][j + 1] == '*') res++;
+    }
+    return res;
+}
+
+int check_left(char ** input, int i, int j) {
+    int res = 0;
+    for(int s = 0; s < 3; s++) {
+        if(input[i - 1 + s][j - 1] == '*') res++;
+    }
+    return res;
+}
+
+int check_bot(char ** input, int i, int j) {
+    if(input[i + 1][j] == '*') return 1;
+    return 0;
+}
+
+int check_top(char ** input, int i, int j) {
+    if(input[i - 1][j] == '*') return 1;
+    return 0;
+}
+
+int check_neighbours(char ** input, int i, int j) {
+    int hf = horizontal_flag(j);
+    int vf = vertical_flag(i);
+    int sum = 0;
+    if(hf == 0 && vf == 0) {
+        sum += check_bot(input, i, j);
+        sum += check_top(input, i, j);
+        sum += check_left(input, i, j);
+        sum += check_right(input, i, j);
+    }
+    return sum;
 }
 
 void output(char ** input) {
@@ -28,7 +79,7 @@ void output(char ** input) {
 
 int main() {
     char ** input;
-    /* for(int i = 0; i < HEIGHT; i++) { */
+    /* for(int i = 0; i < HEIGHT; i++) { // its for generating empty field with ./main > input/empty_input */
     /*     for(int j = 0; j < WIDTH; j++) { */
     /*         printf("*"); */
     /*     } */
@@ -37,6 +88,7 @@ int main() {
     input = memall();
     init(input);
     output(input);
+    printf("%d\n", check_neighbours(input, 10, 10));
     for(int i = 0; i < HEIGHT; i++) free(input[i]);
     free(input);
     return 0;
